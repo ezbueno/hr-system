3d058eb568536d3e28486f3bcc7d95f616f8e85a
ezbueno <ezandrobueno@yahoo.com.br>
2023-04-07 19:55:02 -0300

    payroll api created

--- payroll-api/src/main/java/com/buenoezandro/payrollapi/PayrollApiApplication.java
+++ payroll-api/src/main/java/com/buenoezandro/payrollapi/PayrollApiApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class PayrollApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PayrollApiApplication.class, args);
     }
-
 }
