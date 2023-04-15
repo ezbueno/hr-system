6b4127b03b902492bcbf3efa65ed7a7ff0df8bd1
ezbueno <ezandrobueno@yahoo.com.br>
2023-04-15 16:53:28 -0300

    Load Balancing

--- user-api/src/main/java/com/buenoezandro/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/com/buenoezandro/userapi/services/impl/UserServiceImpl.java
@@ -5,17 +5,22 @@ import com.buenoezandro.userapi.repositories.UserRepository;
 import com.buenoezandro.userapi.services.UserService;
 import com.buenoezandro.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final Environment environment;
 
     @Override
     public User findById(Long id) {
+        log.info("USER SERVICE ::: Get request on " + this.environment.getProperty("local.server.port") + " port");
         return this.userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
